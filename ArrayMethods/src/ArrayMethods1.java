0994d894019f41af999f8036ac9b103137c4c8d5
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2018-01-17 09:29:26 -0500

    jjfish

--- ArrayMethods/src/ArrayMethods1.java
+++ ArrayMethods/src/ArrayMethods1.java
@@ -1,9 +1,21 @@
 
 public class ArrayMethods1 
 {
-	public static void swap (int mama, int mio )
+	public static void swap (int[]arr, int mama, int mio )
 	{
-		
+		int store = arr[mama];
+		arr[mama]= arr[mio];
+		store = arr[mio];
+	}
+	
+	
+	public static void bubbleSort(String[]list1)
+	{
+		int swapnum = 1;
+		while(swapnum)
+		{
+			
+		}
 	}
 
 
