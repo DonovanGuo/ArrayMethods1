7e4d35fe5ffe3728ae2c606152f244ca49f1ac76
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2018-03-07 09:03:54 -0500

    jjfish

--- ArrayMethods/src/ArrayMethods1.java
+++ ArrayMethods/src/ArrayMethods1.java
@@ -73,20 +73,6 @@ public class ArrayMethods1
 			}
 		}
 	}
-	public static void main(String[]args)
-	{
-	int[]test1 = {1,4,4,5,2,4,3,17,0};
-	double[]test2 = {1.1, 4.1 , 4.2 , 5.0 , 2.0 , 4.0 , 3.0 , 17.0 , 0.0};
-	String[]test3 = {"zebra","tortilla","abba","foo","bar","aba"};
-	
-	insertionSort(test1);
-	System.out.println(Arrays.toString(test1));
-	selectionSort(test2);
-	System.out.println(Arrays.toString(test2));
-	bubbleSort(test3);
-	System.out.println(Arrays.toString(test3));
-	
-	}
 
 
 
